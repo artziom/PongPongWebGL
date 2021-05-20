8766cfd100e177c4fba3fc669c5b06fb6aec4c45
ArtZiom <artur.ziomek@triverna.pl>
2021-05-20 09:17:26 +0200

    Add HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: "./src/scripts/app.ts",
     output: {
         filename: "app.js",
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
     },
     mode: "development",
     module: {
@@ -19,5 +21,10 @@ module.exports = {
     resolve: {
         extensions: [".ts", ".js"]
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Webpack App"
+        })
+    ],
     devtool: "inline-source-map"
 };
